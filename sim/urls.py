18d70e026d74fc2018a1ab7d2c3242a6ec28a00b
TOGoS <togos00@gmail.com>
2014-02-15 15:06:45 -0600

    Fight with Django to make JSON responses.

--- sim/urls.py
+++ sim/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 from sim import views
 
 urlpatterns = patterns('',
-    url(r'^transfers$', views.get_transfers, name='transfers')
+    url(r'^transfers$', views.transfers, name='transfers')
 )
--- sim/views.py
+++ sim/views.py
@@ -1,10 +1,19 @@
 import json
 
 from django.shortcuts import render
+import json
+
 from django.http import HttpResponse
 from django.core import serializers
 
 from .models import *
+
+def json_response(stuff):
+    json_serializer = serializers.get_serializer('json')
+    json_serializer = json_serializer()
+    f = json.loads(json_serializer.serialize(stuff))
+    data = map(lambda q: q['fields'], f)
+    return HttpResponse(data, content_type='application/json')
 
 def index(request):
     return HttpResponse("hello")
@@ -18,8 +27,19 @@ def get_exchange_rates_for_exchanger(request, exchanger_name='Coinbase'):
     json_rates = json_serializer.serialize(rates)
     return HttpResponse(json_rates, content_type='json')
 
-def get_transfers(request):
-    return HttpResponse(serializers.serialize('json', sim.models.Transfer.objects.all()), content_type="application/json")
+def transfers(request):
+    return json_response(Transfer.objects.all())
+
+def trade(request):
+    params = request.POST
+    seller = Exchanger.objects.get(id=params['seller_id'])
+    buyer = Exchanger.objects.get(id=params['partner_id'])
+    sell_currency = Currency.objects.get(params['sell_currency_code'])
+    buy_currency = Currency.objects.get(params['buy_currency_code'])
+    params['sell_amount']
+    params['buy_amount']
+    
+    
 
 def post_trade(request):
     return HttpResponse(json.dumps({'hi': 'wat ap'}), content_type="application/json")
