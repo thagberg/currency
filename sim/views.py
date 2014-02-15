a82680e96d2e2c2d2be4440a633464168624f266
TOGoS <togos00@gmail.com>
2014-02-15 14:20:20 -0600

    Add /sim/transfers.

--- sim/views.py
+++ sim/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+import json
+from django.http import HttpResponse
+from django.core import serializers
+import sim
 
-# Create your views here.
+def get_transfers(request):
+    return HttpResponse(serializers.serialize('json', sim.models.Transfer.objects.all()), content_type="application/json")
+
+def post_trade(request):
+    return HttpResponse(json.dumps({'hi': 'wat ap'}), content_type="application/json")
