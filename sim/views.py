779f356ca81d87de0d4db9e7216df5efd6a912d7
thagberg <timothy.m.hagberg@gmail.com>
2014-02-15 14:53:53 -0600

    Fixed up exchange rates view a bit

--- sim/views.py
+++ sim/views.py
@@ -13,8 +13,7 @@ def get_exchange_rates_for_exchanger(request, exchanger_name='Coinbase'):
     json_serializer = serializers.get_serializer('json')
     json_serializer = json_serializer()
     exchanger = Exchanger.objects.get(name=exchanger_name)
-    #rates = ExchangeRate.objects.get(exchanger=exchanger)
-    rates = ExchangeRate.objects.all()
+    rates = ExchangeRate.objects.filter(exchanger=exchanger)
     json_rates = json_serializer.serialize(rates)
     return HttpResponse(json_rates, content_type='json')
 
