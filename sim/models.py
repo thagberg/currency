a26eb0f5145234eabd6f2b92e39dbc1eb01ffd76
thagberg <timothy.m.hagberg@gmail.com>
2014-02-15 10:35:08 -0600

    Added sim app and basic TradePair data model

--- sim/models.py
+++ sim/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class TradingPair(models.Model):
+    crypt_currency = models.CharField(max_length=5)
+    world_currency = models.CharField(max_length=5)
+    current_value = models.DecimalField(max_digits=20, decimal_places=10)
