d7efa3ff01b89cc89d187b9b99e8e6ac86065809
thagberg <timothy.m.hagberg@gmail.com>
2014-02-15 14:54:19 -0600

    Added rates urls

--- sim/urls.py
+++ sim/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import patterns, url
 from sim import views
 
 urlpatterns = patterns('',
-    url(r'^transfers$', views.get_transfers, name='transfers')
+    url(r'^transfers$', views.get_transfers, name='transfers'),
+    url(r'^rates/(?P<exchanger_name>\d+)/$', views.get_exchange_rates_for_exchanger),
+    url(r'^rates/$', views.get_exchange_rates_for_exchanger),
+    url(r'^$', views.index)
 )
