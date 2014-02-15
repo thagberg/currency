4fb555845f5b0908ae79127f45d66dffb5d41736
TOGoS <togos00@gmail.com>
2014-02-15 16:38:46 -0600

    Change trade URLs.

--- sim/urls.py
+++ sim/urls.py
@@ -7,7 +7,6 @@ urlpatterns = patterns('',
     url(r'^users/(?P<user_id>.+)$', views.users),
     url(r'^usertradetriggers$', views.user_trade_triggers),
     url(r'^rates/(?P<exchanger_name>.+)$', views.get_exchange_rates_for_exchanger),
-    url(r'^rates/$', views.get_exchange_rates_for_exchanger),
-    url(r'^trade/$', views.trade),
-    url(r'^$', views.index)
+    url(r'^rates$', views.get_exchange_rates_for_exchanger),
+    url(r'^trades$', views.trade)
 )
