cf5f629e174256ca273ac99a7edeb0d03bab7c0c
thagberg <timothy.m.hagberg@gmail.com>
2014-02-15 14:38:41 -0600

    Added exchange rate view

--- sim/views.py
+++ sim/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
 
-# Create your views here.
+from .models import *
+
+def index(request):
+    return HttpResponse("hello")
+
+def get_exchange_rates_for_exchanger(request, exchanger_name='Coinbase'):
+    json_serializer = serializers.get_serializer('json')
+    json_serializer = json_serializer()
+    exchanger = Exchanger.objects.get(name=exchanger_name)
+    #rates = ExchangeRate.objects.get(exchanger=exchanger)
+    rates = ExchangeRate.objects.all()
+    json_rates = json_serializer.serialize(rates)
+    return HttpResponse(json_rates, content_type='json')
